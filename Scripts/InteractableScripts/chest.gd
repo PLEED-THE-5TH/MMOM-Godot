7b7fe6b2cbe999ec6091ea198b50b111aef48b39
Stone Hobbs <stoneh826@gmail.com>
2023-12-04 18:41:39 -0600

    Goodbye World

--- Scripts/InteractableScripts/chest.gd
+++
@@ -1,8 +0,0 @@
-extends StaticBody3D
-
-signal toggle_inventory(external_inventory_owner)
-
-@export var inventory_data: InventoryData
-
-func player_interact() -> void:
-	toggle_inventory.emit(self)
--- Scripts/PlayerScripts/Camera_Mount.gd
+++
@@ -1,4 +0,0 @@
-extends Node3D
-
-func _process(_delta):
-	rotation.x = clamp(rotation.x, -1.4, 1.4)
